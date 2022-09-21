cb7d31cef9b8c58b6cd7079e22d6093f393c7992
NatusAnima <i.yavets@gmail.com>
2022-09-21 14:29:03 +0300

    Add functionality to all buttons

--- scripts.js
+++ scripts.js
@@ -8,9 +8,20 @@ multiply = (a, b) => a * b;
 
 divide = (a, b) => a / b;
 
-operator = (operator, a, b) => operator(a, b);
-
-
+function operator(operator, a, b){
+    if (operator === '+'){
+        return sum(a,b);
+    }
+    else if (operator === '-'){
+        return substract(a,b);
+    }
+    else if (operator === '*'){
+        return multiply(a,b);
+    }
+    else if (operator === '/'){
+        return divide(a,b);
+    }
+}
 
 const displayedNumber = document.getElementById("displayedNumber");
 window.addEventListener('keydown', function (e) {
@@ -18,7 +29,8 @@ window.addEventListener('keydown', function (e) {
     console.log(e.key);
 });
 
-
+let currentNumber = 0;
+let operatorSign = 0;
 
 function actions(key){
     if (key >= 0 || key <= 9) {
@@ -38,17 +50,32 @@ function actions(key){
         }
     }
 
+    if (key === 'Clear'){
+        clearScreen();
+        currentNumber = 0;
+    }
+
+    if (key === '+' || key === '-' || key === '*' || key === '/'){
+        if(operatorSign != 0){
+            currentNumber = operator(operatorSign, currentNumber, parseInt(displayedNumber.innerText));
+            displayedNumber.innerText = currentNumber;
+            operatorSign = 0;
+        }
+        operatorSign = key;
+        currentNumber = parseInt(displayedNumber.innerText);
+        clearScreen();
+    }
     
 
+    if (key === '='){
+        currentNumber = operator(operatorSign, currentNumber, parseInt(displayedNumber.innerText));
+        displayedNumber.innerText = currentNumber;
+        operatorSign = 0;
+     }
+}
 
-
-
-
-    if (key === 'Clear'){
-        displayedNumber.innerText = 0;
-        result = 0;
-        displayValue = 0;
-    }
+function clearScreen(){
+    displayedNumber.innerText = "0";
 
 }
 
