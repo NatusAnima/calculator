a13ed5869d08b1d0c7dbc6e300bdfee8dad4485b
NatusAnima <i.yavets@gmail.com>
2022-09-21 13:38:56 +0300

    Change User Interface

--- scripts.js
+++ scripts.js
@@ -15,15 +15,13 @@ operator = (operator, a, b) => operator(a, b);
 const displayedNumber = document.getElementById("displayedNumber");
 window.addEventListener('keydown', function (e) {
     actions(e.key);
+    console.log(e.key);
 });
 
-let displayValue = 0;
-let previousValue = 0;
-let result = 0;
+
 
 function actions(key){
     if (key >= 0 || key <= 9) {
-        console.log(key);
         if (displayedNumber.innerText == "0"){
             displayedNumber.innerText = displayedNumber.innerText.slice(0, -1);
         }
@@ -40,32 +38,16 @@ function actions(key){
         }
     }
 
-    if (key === '+'){
-        displayValue = parseInt(displayedNumber.innerText);
-        result = operator(sum, displayValue, result);
-        displayedNumber.innerText = result;
-    }
+    
 
-    if (key === '-'){
-        displayValue = parseInt(displayedNumber.innerText);
-        result = operator(substract, displayValue, result);
-        displayedNumber.innerText = result;
-    }
 
-    if (key === '/'){
-        displayValue = parseInt(displayedNumber.innerText);
-        result = operator(divide, displayValue, result);
-        displayedNumber.innerText = result;
-    }
 
-    if (key === '*'){
-        displayValue = parseInt(displayedNumber.innerText);
-        result = operator(multiply, displayValue, result);
-        displayedNumber.innerText = result;
-    }
 
-    if (key === '=' || key=== 'Enter'){
-        displayedNumber.innerText = result;
+
+    if (key === 'Clear'){
+        displayedNumber.innerText = 0;
+        result = 0;
+        displayValue = 0;
     }
 
 }
